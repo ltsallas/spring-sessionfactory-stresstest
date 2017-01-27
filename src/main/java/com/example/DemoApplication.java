f18cc3400f0fe58a7601e6ffd34e0737833c84ba
tsallase <eleftherios.tsallas@trasysgroup.com>
2017-01-27 18:19:52 +0200

    Use two transaction managers

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -1,20 +1,30 @@
 package com.example;
 
+import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 
 import com.zaxxer.hikari.HikariDataSource;
 import org.hibernate.SessionFactory;
-import org.hibernate.jpa.HibernateEntityManagerFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
 import org.springframework.context.ApplicationContextException;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBuilder;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
 
 @SpringBootApplication
 @ComponentScan("com.example")
+@EnableJpaRepositories(entityManagerFactoryRef = "jpaEntityManagerFactory", basePackages = "com.example.repository")
 public class DemoApplication {
 
 	public static void main(String[] args) {
@@ -40,7 +50,57 @@ public class DemoApplication {
 	}
 
 	@Bean
-	public SessionFactory sessionFactory(HibernateEntityManagerFactory hemf){
-		return hemf.getSessionFactory();
+	public SessionFactory sessionFactory(DataSourceProperties dataSourceProperties) {
+		LocalSessionFactoryBuilder sessionBuilder = new LocalSessionFactoryBuilder(dataSource(dataSourceProperties));
+		sessionBuilder
+				.scanPackages("com.example.domain")
+				.setProperty("hibernate.show_sql", "true")
+            .setProperty("hibernate.hbm2ddl.auto", "create")
+//                .setProperty("hibernate.jdbc.use_streams_for_binary", "false")
+				.setProperty("hibernate.query.timeout", "180")
+				.setProperty("hibernate.default_batch_fetch_size", "100")
+				.setProperty("hibernate.jdbc.batch_size", "30")
+				.setProperty("hibernate.sessionfactory.reuse", "true")
+				.setProperty("hibernate.order_updates", "true")
+				.setProperty("hibernate.order_inserts", "true")
+
+				// database dependent configuration
+				// only for Oracle (as it has no boolean support)
+				.setProperty("hibernate.query.substitutions", "true 1, false 0");
+		return sessionBuilder.buildSessionFactory();
+	}
+
+	@Bean
+	@Qualifier(value = "hibernateTransactionManager")
+	@Primary
+	public HibernateTransactionManager hibernateTransactionManager(DataSourceProperties dataSourceProperties) {
+		HibernateTransactionManager transactionManager =
+				new HibernateTransactionManager();
+		transactionManager.setSessionFactory(sessionFactory(dataSourceProperties));
+		return transactionManager;
+	}
+
+	@Bean(name = "jpaEntityManagerFactory")
+	@Qualifier("jpaEntityManagerFactory")
+	public LocalContainerEntityManagerFactoryBean jpaEntityManagerFactory(
+			EntityManagerFactoryBuilder builder,
+			@Qualifier("dataSource") DataSource dataSource) {
+
+		return builder
+				.dataSource(dataSource)
+				.packages("com.example.domain")
+				.persistenceUnit("foo")
+				.build();
+	}
+//
+//	@Bean(name = "jpaTransactionManager")
+//	public PlatformTransactionManager jpaTransactionManager() {
+//		return jpaTransactionManager();
+//	}
+
+	@Bean(name = "transactionManager")
+	public PlatformTransactionManager transactionManager(
+			@Qualifier("jpaEntityManagerFactory") EntityManagerFactory jpaEntityManagerFactory) {
+		return new JpaTransactionManager(jpaEntityManagerFactory);
 	}
 }
